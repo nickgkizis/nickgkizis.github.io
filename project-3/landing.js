05f3d523c4c1d40ec47da6b72605a69349a913b1
nick gkizis <nikolaosgkizis@gmail.com>
2024-07-24 11:14:11 +0300

    updates

---
+++ project-3/landing.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let box1 = document.querySelector('.b2');
+    let box2 = document.querySelector('.b1');
+    let hidden = document.getElementById('hide');
+    let cont = document.getElementById('contn');
+
+    box1.addEventListener('mouseover', function() {
+        hidden.innerHTML = 'Want to join us?';
+        // hidden.style.opacity=1;
+        cont.style.background =' linear-gradient(to left bottom, #d53ac5 0%, #0c2aec 100%)';
+    });
+
+    box1.addEventListener('mouseout', function() {
+        hidden.innerHTML = 'Welcome to my Website !';
+        // hidden.style.opacity=0;
+        cont.style.background = 'linear-gradient(to bottom, #d53ac5 0%, #0c2aec 100%)';
+        
+    });
+
+    box2.addEventListener('mouseover', function() {
+        hidden.innerHTML = 'Already have an account?';
+        // hidden.style.opacity=1;
+        cont.style.background =' linear-gradient(to right bottom, #d53ac5 0%, #0c2aec 100%)';
+
+    });
+
+    box2.addEventListener('mouseout', function() {
+        hidden.innerHTML = 'Welcome to my Website !';
+        // hidden.style.opacity=0;
+        cont.style.background = 'linear-gradient(to bottom, #d53ac5 0%, #0c2aec 100%)';
+    });
+});
